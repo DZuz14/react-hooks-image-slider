bc1b251acba83abe58ff03e65e6a9d19af481736
Daniel Zuzevich <dzuzevich@graftonstudio.com>
2018-03-22 20:56:13 -0400

    tidied up redux store

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import reducers from './reducers'
 import reduxThunk from 'redux-thunk'
-
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore)
 const store = createStoreWithMiddleware(reducers)
 
